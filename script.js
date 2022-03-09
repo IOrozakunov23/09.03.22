a9a925700c762762597821301b7bc761ac554341
IOrozakunov23 <islamburunduk045>
2022-03-09 14:29:02 +0300

    added 3 files

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const main = document.querySelector("main");
 const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function displayList() {
+  main.innder.HTML = "  "
   list.forEach((item) => {
     const div = document.createElement("div");
     div.textContent = item;
@@ -9,5 +10,9 @@ function displayList() {
   });
 }
 
-displayList();
+function sortDescending(){
+
+} 
+
+sortDescending();
 displayList();
