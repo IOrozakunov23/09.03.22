4fcf76520001c8546c5d890d245af2c620624d96
IOrozakunov23 <islamburunduk045>
2022-03-09 14:22:15 +0300

    added 3 files

---
+++ script.js
@@ -0,0 +1,13 @@
+const main = document.querySelector("main");
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function displayList() {
+  list.forEach((item) => {
+    const div = document.createElement("div");
+    div.textContent = item;
+    main.append(div);
+  });
+}
+
+displayList();
+displayList();
